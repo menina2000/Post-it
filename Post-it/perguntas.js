602d8c68b8ffb70bd998c9cb41bda4739f71a9ce
Carol2000 <168771875+menina2000@users.noreply.github.com>
2024-11-29 16:23:01 -0300

    Add files via upload

---
+++ Post-it/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Matemática',
+    'O que é o Teorema de Pitágoras?',
+    'O Teorema de Pitágoras relaciona o comprimento dos lados do triângulo retângulo, cujo é formada por um ângulo interno de 90°, chamado de ângulo reto. A fórmula a²=b²+c² é usada para determinar a medida desconhecida de um lado, uma vez conhecidas as medidas dos outros dois lados.'
+)
+
+criaCartao(
+    'Física',
+    'O que é Termodinâmica?',
+    'A termodinâmica é uma área da Física que estuda as transferências de energia. Busca compreender as relações entre calor, energia e trabalho, analisando quantidades de calor trocadas e os trabalhos realizados em um processo físico.'
+)
+
+criaCartao(
+    'Biologia',
+    'O que é uma célula?',
+    'A célula é a unidade estrutural e funcional dos seres vivos, que podem ser formados por uma única célula, no caso dos unicelulares, ou várias células associadas, os multicelulares.'
+)
+
+criaCartao(
+    'Química',
+    'O que é matéria?',
+    'A matéria é o objeto de estudo da Química, que por sua vez, estabelece as relações entre a sua constituição, propriedades e transformações. Vale lembrar que matéria é tudo aquilo que tem massa e ocupa um lugar no espaço. Um material é composto por átomos, que se agrupam e formam as diferentes substâncias químicas existentes.'
+)
